112def3020d0c0a959c49a45c6f538e5ac885c5b
jonguzman5 <student_05@PSXGA105.local>
2016-10-05 10:50:42 -0400

    Final Project

---
+++ WayneEnterprisesJavaScript.js
@@ -0,0 +1,46 @@
+function openNav() {
+    'use strict';
+    document.getElementById("rightnav").style.width = "100%";
+}
+function closeNav() {
+    'use strict';
+    document.getElementById("rightnav").style.width = "0";
+}
+window.onload = function(){
+    var promo = document.getElementById("WaynePromo");
+    var play = document.getElementById("WaynePromo");
+    play.addEventListener("click",function(){promo.play();
+    });   
+   }
+/*-----------------------------*/
+function move() {
+  var elem = document.getElementById("myAnimation");
+  var pos = 0;
+  var id = setInterval(frame, 5);
+function frame() {
+  if (pos == 1140) {
+    clearInterval(id);
+  } else {
+    pos++;
+    elem.style.left = pos + 'px';
+  }
+ } 
+}
+/*---------------------------------*/
+var organization = 0;
+organize(organization);
+
+function addDivs(n) {
+    organize(organization += n);
+}
+function organize(n) {
+    var i;
+    var x = document.getElementsByClassName("wayneexec");
+    if (n > x.length) {organization = 1}
+    if (n < 1) {organization = x.length}
+    for (i=0; i<x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[organization-1].style.display = "block";
+    }
+/*----------------------------------*/
---
+++ WayneHeaderJavaScript.js
@@ -0,0 +1,8 @@
+function navopen() {
+    'use strict';
+    document.getElementById("nav").style.width = "100%";
+}
+function navclose() {
+    'use strict';
+    document.getElementById("nav").style.width = "0";
+}
